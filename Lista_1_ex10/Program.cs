e34e336a34673709a60f4d66f9d7eeddf0268834
mentarios <matmentarios@gmail.com>
2024-02-27 14:52:32 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.WriteLine("digite seu ano de nascimento:");
+            idade = int.Parse(Console.ReadLine());
+            int id = 2024 - idade; Console.WriteLine("sua idade é: " + id);
+            if (id >= 16)
+            {
+                Console.WriteLine("voce pode votar!");
+            }
+            if (id >= 18)
+            {
+                Console.WriteLine("voce pode tirar C.N.H.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
